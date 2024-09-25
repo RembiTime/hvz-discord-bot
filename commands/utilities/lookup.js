0498a27680d502574606f5ab158278d5b5d0e4b6
Rembi <rembitime@gmail.com>
2024-09-25 14:28:43 -0400

    made the lookup command ephemeral (and added an option to not)

--- commands/utilities/lookup.js
+++ commands/utilities/lookup.js
@@ -11,7 +11,11 @@ module.exports = {
 			option.setName('name')
 			.setDescription('The name of the blaster to look up')
 			.setRequired(true)
-			.setAutocomplete(true)),
+			.setAutocomplete(true))
+		.addBooleanOption(option =>
+			option.setName('public')
+			.setDescription('If the output should display to other players (default is no)')
+			.setRequired(false)),
 	async autocomplete(interaction) {
 		await interaction.respond(
             lib.autocompleteSearch(interaction),
@@ -25,12 +29,12 @@ module.exports = {
 			await interaction.reply('**' + interaction.options.getString('name') + '** was not found in the database. Please use \`\/check blaster\` to get it approved!')
 		} else {
 			switch (blaster.tier) {
-				case -1: await interaction.reply('The **' + blaster.name + '** is currently pending review. Please check back later.'); break;
-				case 1: await interaction.reply('The **' + blaster.name + '** has been **approved** for play. It can be used any time.'); break;
-				case 2: await interaction.reply('The **' + blaster.name + '** has been **approved** for play. It can be used any time.'); break;
-				case 3: await interaction.reply('The **' + blaster.name + '** has been **banned** from play. It can **not** be used for Humans vs. Zombies.'); break;
-				case 4: await interaction.reply('The **' + blaster.name + '** is a **BANNED** blaster. It can **not** be used for Humans vs. Zombies.'); break;
-				default: await interaction.reply('Something went wrong. Please contact someone.'); break;
+				case -1: await interaction.reply({content: 'The **' + blaster.name + '** is currently pending review. Please check back later.', ephemeral: !interaction.options.getBoolean('public') ?? true}); break;
+				case 1: await interaction.reply({content: 'The **' + blaster.name + '** has been **approved** for play. It can be used any time.', ephemeral: !interaction.options.getBoolean('public') ?? true}); break;
+				case 2: await interaction.reply({content: 'The **' + blaster.name + '** has been **approved** for play. It can be used any time.', ephemeral: !interaction.options.getBoolean('public') ?? true}); break;
+				case 3: await interaction.reply({content: 'The **' + blaster.name + '** has been **banned** from play. It can **not** be used for Humans vs. Zombies.', ephemeral: !interaction.options.getBoolean('public') ?? true}); break;
+				case 4: await interaction.reply({content: 'The **' + blaster.name + '** is a **BANNED** blaster. It can **not** be used for Humans vs. Zombies.', ephemeral: !interaction.options.getBoolean('public') ?? true}); break;
+				default: await interaction.reply({content: 'Something went wrong. Please contact someone.', ephemeral: true}); break;
 			}
 		} 
 	},
